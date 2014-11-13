f94cbff2c357af27b2a8cd5000396d88d271715c
Aaron Olson <zat1gi@yahoo.com>
2014-11-13 15:03:44 -0700

    Paper: LP, 1, 2, 3, 5, AM leakage results for ANS14

---
+++ many.sh
@@ -0,0 +1,27 @@
+cp inputstoc1_eig1.txt inputstoc.txt
+./bashcommand.sh > many1_eig1.print
+cp inputstoc2_eig1.txt inputstoc.txt
+./bashcommand.sh > many2_eig1.print
+cp inputstoc3_eig1.txt inputstoc.txt
+./bashcommand.sh > many3_eig1.print
+
+cp inputstoc1_eig2.txt inputstoc.txt
+./bashcommand.sh > many1_eig2.print
+cp inputstoc2_eig2.txt inputstoc.txt
+./bashcommand.sh > many2_eig2.print
+cp inputstoc3_eig2.txt inputstoc.txt
+./bashcommand.sh > many3_eig2.print
+
+cp inputstoc1_eig3.txt inputstoc.txt
+./bashcommand.sh > many1_eig3.print
+cp inputstoc2_eig3.txt inputstoc.txt
+./bashcommand.sh > many2_eig3.print
+cp inputstoc3_eig3.txt inputstoc.txt
+./bashcommand.sh > many3_eig3.print
+
+cp inputstoc1_eig5.txt inputstoc.txt
+./bashcommand.sh > many1_eig5.print
+cp inputstoc2_eig5.txt inputstoc.txt
+./bashcommand.sh > many2_eig5.print
+cp inputstoc3_eig5.txt inputstoc.txt
+./bashcommand.sh > many3_eig5.print
